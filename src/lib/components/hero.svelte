6c48375ab7965fa50e3a32cd572e1b51c4b1a0ee
f1rq <f.czudaj@tutanota.com>
2025-10-03 22:10:05 +0200

    Changed colors.

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -29,13 +29,13 @@
 
 
 
-<div class="hero min-h-screen bg-[#FAF7F5]">
+<div class="hero min-h-screen bg-base-100">
     <div class="hero-content flex-col items-start pt-48">
-        <h1 class="text-9xl font-bold text-[#1D232A]">f1rq</h1>
+        <h1 class="text-9xl font-bold text-primary">f1rq</h1>
         <div class="flex flex-row gap-36 items-baseline">
             <div class="flex flex-col">
-                <p class="py-2 max-w-lg text-4xl text-[#1D232A]">{$_('main1')}</p>
-                <div class="flex flex-row gap-1">
+                <p class="py-2 max-w-lg text-4xl text-base-content">{$_('main1')}</p>
+                <!--<div class="flex flex-row gap-1">
                     {#each links as link}
                         <a href={link.href} target="_blank">
                             <div class="h-8 w-8 fill-[#1D232A]">
@@ -43,11 +43,11 @@
                             </div>
                         </a>
                     {/each}
-                </div>
+                </div>-->
             </div>
             <div class="flex flex-col gap-4">
-                <span class="text-sm md:max-w-md text-[#1D232A]">{$_('main2')}</span>
-                <span class="text-sm md:max-w-md text-[#1D232A]">{$_('main3')}</span>
+                <span class="text-sm md:max-w-md text-base-content">{$_('main2')}</span>
+                <span class="text-sm md:max-w-md text-base-content">{$_('main3')}</span>
             </div>
         </div>
     </div>
