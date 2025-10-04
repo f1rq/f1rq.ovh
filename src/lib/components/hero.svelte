6fb80be77a82802ee0a5bd32a2fe508e624c272c
f1rq <f.czudaj@tutanota.com>
2025-10-04 10:39:45 +0200

    Removed unnecessary links from main page.

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,30 +1,5 @@
 <script>
     import { _ } from 'svelte-i18n'
-
-    import github_logo_svg from '$lib/assets/svg-icons/github_logo.svg?raw'
-    import discord_logo_svg from '$lib/assets/svg-icons/discord_logo.svg?raw'
-    import spotify_logo_svg from '$lib/assets/svg-icons/spotify_logo.svg?raw'
-    import coffe_logo_svg from '$lib/assets/svg-icons/coffe_logo.svg?raw'
-
-    const links = [
-        {
-            href: "https://github.com.f1rq",
-            logoSvg: github_logo_svg,
-        },
-        {
-            href: "https://buymeacoffee.com/f1rq",
-            logoSvg: coffe_logo_svg
-        },
-        {
-            href: "https://open.spotify.com/user/dcfv3p5qxwa0sfdimxgyqabri",
-            logoSvg: spotify_logo_svg,
-        },
-        {
-            href: "https://discord.com/invite/YU5yWFt7vB",
-            logoSvg: discord_logo_svg,
-        },
-
-    ]
 </script>
 
 
@@ -35,15 +10,6 @@
         <div class="flex flex-row gap-36 items-baseline">
             <div class="flex flex-col">
                 <p class="py-2 max-w-lg text-4xl text-base-content">{$_('main1')}</p>
-                <!--<div class="flex flex-row gap-1">
-                    {#each links as link}
-                        <a href={link.href} target="_blank">
-                            <div class="h-8 w-8 fill-[#1D232A]">
-                                {@html link.logoSvg}
-                            </div>
-                        </a>
-                    {/each}
-                </div>-->
             </div>
             <div class="flex flex-col gap-4">
                 <span class="text-sm md:max-w-md text-base-content">{$_('main2')}</span>
