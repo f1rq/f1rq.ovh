c3fc25eb96ac1e7d5b4ce4e171d9f69e756ae0ff
Fabio <forgistm.pl@gmail.com>
2025-10-05 00:57:19 +0200

    Fixed mobile layout.

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -7,9 +7,9 @@
 <div class="hero min-h-screen bg-base-100">
     <div class="hero-content flex-col items-start pt-48">
         <h1 class="text-9xl font-bold text-primary">f1rq</h1>
-        <div class="flex flex-row gap-36 items-baseline">
+        <div class="flex flex-col gap-4 md:flex-row md:gap-36 items-baseline">
             <div class="flex flex-col">
-                <p class="py-2 max-w-lg text-4xl text-base-content">{$_('main1')}</p>
+                <p class="py-2 max-w-lg text-2xl md:text-4xl text-base-content">{$_('main1')}</p>
             </div>
             <div class="flex flex-col gap-4">
                 <span class="text-sm md:max-w-md text-base-content">{$_('main2')}</span>
