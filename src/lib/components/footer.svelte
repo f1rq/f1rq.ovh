8416fc877ce6f9ce2207a540a74673da1527530d
Fabio <forgistm.pl@gmail.com>
2025-10-05 00:55:45 +0200

    Add footer component.

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<footer class="footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4">
+    <aside>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved by <span class="text-primary">f1rq</span></p>
+    </aside>
+</footer>
